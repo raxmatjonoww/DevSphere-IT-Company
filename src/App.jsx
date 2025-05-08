b36ec22d92bcb38166330c0aad1bcb66894a96e5
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-05-08 20:19:46 +0500

    DevSphere

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer'
-import Container from './Components/Container/Container'
 
 const App = () => {
   return (
     <Router>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Container />} />
       </Routes>
+      <Footer/>
     </Router>
   );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import './Navbar.css';
-import { Link } from 'react-router-dom'; // Для React Router
+import React, { useState } from "react";
+import "./navbar.css";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <a href="/">DevSphere</a>
+      <div className="navbar-container">
+        <h1 className="logo">DevSphere</h1>
+
+        <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+          <li><a href="#home">Главная</a></li>
+          <li><a href="#about">О нас</a></li>
+          <li><a href="#services">Услуги</a></li>
+          <li><a href="#projects">Проекты</a></li>
+          <li><a href="#contact">Контакты</a></li>
+        </ul>
+
+        <div className="menu-icon" onClick={toggleMenu}>
+          {isOpen ? <X size={28} color="#fff" /> : <Menu size={28} color="#fff" />}
+        </div>
       </div>
     </nav>
   );

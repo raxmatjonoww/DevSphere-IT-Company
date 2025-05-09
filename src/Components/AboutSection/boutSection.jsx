904ca57a5fae0cfa7b89cfb2aabadb671162e6b5
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-05-10 00:04:00 +0500

    Dev

--- src/Components/AboutSection/boutSection.jsx
+++ src/Components/AboutSection/boutSection.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import './AboutSection.css'; // Если вы используете CSS Modules
-// import aboutUsImage from '../../assets/about-us.jpg';
+import './AboutSection.css';
+import comanda from '../../assets/comanda.png';
 
 function AboutSection() {
   return (
     <section className="about-section">
-      <div className="container">
-        <div className="image-container">
-          {/* <img src={aboutUsImage} alt="О нас" /> */}
+      <div className="about-container">
+        <div className="about-image">
+          <img src={comanda} alt="О нас" />
         </div>
-        <div className="text-container">
-          <h2>О DevSphere IT</h2>
-          <p>DevSphere IT - это команда опытных и увлеченных профессионалов в области информационных технологий. Мы стремимся создавать инновационные и эффективные решения, которые помогают нашим клиентам достигать своих бизнес-целей. Наш подход основан на глубоком понимании потребностей клиента, использовании передовых технологий и стремлении к высочайшему качеству.</p>
-          <p>Мы специализируемся на разработке веб-сайтов, мобильных приложений, программного обеспечения, а также предлагаем услуги в области цифрового маркетинга и IT-консалтинга. Наша миссия - быть надежным партнером в вашем цифровом развитии.</p>
+        <div className="about-text">
+          <h2>О DevSphere IT Company</h2>
+          <p>
+            DevSphere IT Company — это команда опытных и увлеченных профессионалов в области информационных технологий. Мы стремимся создавать инновационные и эффективные решения, которые помогают нашим клиентам достигать своих бизнес-целей.
+          </p>
+          <p>
+            Мы специализируемся на разработке веб-сайтов, мобильных приложений, программного обеспечения, а также предлагаем услуги в области цифрового маркетинга и IT-консалтинга.
+          </p>
           <button className="learn-more-button">Узнать больше о компании</button>
         </div>
       </div>

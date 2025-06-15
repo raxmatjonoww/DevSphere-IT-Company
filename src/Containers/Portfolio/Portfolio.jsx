dddd99839199686b03f93b97b5127823a11bb83d
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-06-16 00:52:07 +0500

    DevSphere IT Company

--- src/Containers/Portfolio/Portfolio.jsx
+++ src/Containers/Portfolio/Portfolio.jsx
@@ -1,28 +1,63 @@
 import React from "react";
 import "./Portfolio.css";
 
-// import project1 from "../../assets/kia.png";
-// import project2 from "../../assets/chillpro.png";
-// import project3 from "../../assets/fasten.png";
+import kia from '../../assets/kia.jpg';
+import chillpro from '../../assets/chillpro.jpg';
+import fasten from '../../assets/fasten.jpg';
+import sky from '../../assets/sky.jpg';
+import weestep from '../../assets/weestep.jpg';
+import tomato from '../../assets/tomato.jpg';
+import surfing from '../../assets/surfing.jpg'
+import ers from '../../assets/ers.jpg'
 
 const projects = [
   {
     title: "KIA Uzbekistan",
-    // image: project1,
-    description: "Корпоративный сайт для автомобильной компании.",
+    image: kia,
+    // description: "Корпоративный сайт для автомобильной компании.",
     link: "https://kia.uz",
   },
   {
     title: "ChillPro Service",
-    // image: project2,
-    description: "Сайт услуг по ремонту и установке кондиционеров.",
-    link: "#",
+    image: chillpro,
+    // description: "Сайт услуг по ремонту и установке кондиционеров.",
+    link: "https://www.chillpro.uz/",
   },
   {
     title: "Fasten Marketing",
-    // image: project3,
-    description: "Landing page для маркетингового агентства.",
-    link: "#",
+    image: fasten,
+    // description: "Landing page для маркетингового агентства.",
+    link: "https://fasten.com/ru_uz",
+  },
+  {
+    title: "Skywings",
+    image: sky,
+    // description: "Landing page для Авиа Компании.",
+    link: "https://skywings-studio.vercel.app/",
+  },
+  {
+    title: "Weestep be bigger",
+    image: weestep,
+    // description: "Landing page для маркетингового агентства.",
+    link: "https://weestep-app.vercel.app/",
+  },
+  {
+    title: "Cafe Tomato",
+    image: tomato,
+    // description: "Landing page для маркетингового агентства.",
+    link: "https://tomato-cafe.vercel.app/",
+  },
+  {
+    title: "Surfing School",
+    image: surfing,
+    // description: "Landing page для маркетингового агентства.",
+    link: "https://surfing-school.vercel.app/",
+  },
+  {
+    title: "Ерсұлтан Бекет",
+    image: ers,
+    // description: "Landing page для маркетингового агентства.",
+    link: "https://ersultan-beket.vercel.app/",
   },
 ];
 
